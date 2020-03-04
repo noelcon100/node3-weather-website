d4becbf58acf834469e232a002c00dc09dca0f00
Noel <noelcon100@gmail.com>
2020-03-04 14:47:09 +0000

    init commit

---
+++ src/utils/geocode.js
@@ -0,0 +1,37 @@
+const request = require('request')
+
+const geocode = (address, callback) => {
+
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1Ijoibm9lbGNvbjEwMCIsImEiOiJjazQ0MjRxd2kwNjlwM25uMHBoZ2hob212In0.-OqeGusIEslKw5NxTuSHdA&limit=1'  
+
+request({ url, json: true }, (error, { body }) => {
+    
+
+
+if (error) {
+        callback('Unable to connect to location service', undefined)
+
+} else if (body.features.length === 0) {
+    callback('Unable to find location', undefined)
+
+  }  else {
+    callback(undefined, {
+
+latitude: body.features[0].center[1],
+longitude: body.features[0].center[0],
+location: body.features[0].place_name
+
+
+
+
+    })
+}
+})
+
+}
+
+module.exports = geocode
+//console.log(response.body.daily.data[2].summary + 'It is currently ' + response.body.currently.temperature + ' degrees out, There is a ' + response.body.currently.precipProbability +  '% chance of rain')
+
+
+
